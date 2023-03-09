dc4927f854c5b9cfe851a91d68c2916fed74b09e
Sai Harish <2100031942@kluniversity.in>
2023-03-09 22:47:36 +0530

    This can send Mails to the Users

--- MayaVI/settings.py
+++ MayaVI/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
+
 INSTALLED_APPS = [
     'maya.apps.MayaConfig',
     'django.contrib.admin',
@@ -129,3 +131,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mayaviclubklu@gmail.com'
+EMAIL_HOST_PASSWORD = 'wyhehklwndoqchbp'
--- maya/views.py
+++ maya/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User,auth
 from datetime import datetime
-from .models import Contact
+from .models import Joinus
+from django.core.mail import send_mail
 # Create your views here.
 
 
@@ -14,17 +15,28 @@ def base(request):
 def gallery(request):
      return render(request, "gallery.html")
 
+from django.core.mail import send_mail
+from .models import Joinus
+from datetime import datetime
+
 def joinus(request):
-     if request.method == "POST":
-          first_name = request.POST('first_name')
-          last_name = request.POST('last_name')
-          id_number = request.POST('id_number')
-          email = request.POST('email')
-          comment = request.POST('comment')
-          joinus = Contact(first_name=first_name, last_name=last_name,
-                          id_number=id_number, email=email, comment=comment,date=datetime.today())
-          joinus.save()
-     return render(request, "joinus.html")
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        id_number = request.POST.get('id_number')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
+        joinus = Joinus(first_name=first_name, last_name=last_name, id_number=id_number, email=email, comment=comment, date=datetime.today())
+        joinus.save()
+        
+        subject = 'Congratulations on Joining the MayaVI Club!'
+        message = f"Dear {id_number},\n\nWe are excited to welcome you to the MayaVI Club!\n\nAs a member of the club, you'll have the opportunity to learn about Virtual Reality, Augmented Reality, and Game Development. We can't wait to see what you'll create!\n\nBest regards,\nThe MayaVI Club Team"
+        send_mail(subject, message, 'mayaviclubklu@gmail.com', [email], fail_silently=False)
+
+        return render(request, "success.html")
+    else:
+        return render(request, "joinus.html")
+
 
 
 def projects(request):
